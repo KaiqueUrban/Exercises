58cce9989c6a14b0cfaa5a1a5d7c647b55e44499
Kaique Paulo Esteves Urban <kaique.urban@uni9.edu.br>
2025-11-09 17:59:58 -0300

    Atualizar o secao2_exercicio2.py

--- secao2_exercicio2.py
+++ secao2_exercicio2.py
@@ -2,7 +2,7 @@
 
 #entradas
 num1 = int(input("Insira o primeiro dígito "))
-num2 = int(input("Insita o segundo dígito "))
+num2 = int(input("Insira o segundo dígito "))
 #processamento
 soma = num1 + num2
 multiplicacao = soma * num1
